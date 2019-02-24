6be6cdfdf0a2ccba8cacebf492b930e7035d869d
HighCWu <HighCWu@163.com>
2019-02-24 09:13:26 +0800

    support for tf release 1.13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='keras-bert-tpu',
-    version='0.1.6',
+    version='0.1.7',
     packages=find_packages(),
     url='https://github.com/HighCWu/keras-bert-tpu',
     license='MIT',
@@ -12,7 +12,7 @@ setup(
     long_description=open('README.rst', 'r').read(),
     install_requires=[
         'numpy>=1.15.4',
-        'tf-nightly<=1.13.0.dev20181218', # After tensorflow 1.3.0 release, it will change back to 'tensorflow>=1.3.0'
+        'tensorflow>1.12,<2.0',
     ],
     classifiers=(
         "Programming Language :: Python :: 2.7",
